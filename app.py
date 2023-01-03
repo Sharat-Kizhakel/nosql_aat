c4cc8b6002909c39a67799db7a22b9070e826e4c
pranav <pranava.sri@gmail.com>
2023-01-03 04:19:34 +0000

    connected to db with python

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, jsonify
-from config import IP, PORT, DEBUG
+from config import IP, PORT, DEBUG, DB_UNAME, DB_URI
+from neo4j import GraphDatabase
+import os
 
 app = Flask(__name__)
+graphdb = GraphDatabase.driver(DB_URI,
+                              auth=(DB_UNAME, os.environ['DB_PSWD']))
+session=graphdb.session()
+query="LOAD CSV FROM 'file:///covid_data.csv' AS line CREATE (:PATIENT {Province:line[0],Country:line[1],Lat:line[2],Long:line[3],Date:line[4],Confirmed:line[5],Deaths:line[6],Recovered:line[7],Active:line[8],WHO_Region:line[9]})" 
+print(session.run(query))
 
 @app.get('/')
 def home():
--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 IP = 'localhost'
 PORT = '8080'
 DEBUG = True
+DB_UNAME = 'neo4j'
+DB_URI = 'neo4j://localhost:7687'
 
 # To start and stop neo4j
 '''
