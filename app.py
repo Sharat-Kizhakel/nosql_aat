020405a71034e4002e51e4869c794ebbb424f6cd
pranav <pranava.sri@gmail.com>
2023-01-09 06:27:22 +0000

    added dummy html template

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from config import IP, PORT, DEBUG, DB_UNAME, DB_URI
 from neo4j import GraphDatabase
 import os
 
 app = Flask(__name__)
-graphdb = GraphDatabase.driver(DB_URI,
-                              auth=(DB_UNAME, os.environ['DB_PSWD']))
+graphdb = GraphDatabase.driver(DB_URI, auth=(DB_UNAME, os.environ['DB_PSWD']))
 session=graphdb.session()
-query="LOAD CSV FROM 'file:///covid_data.csv' AS line CREATE (:PATIENT {Province:line[0],Country:line[1],Lat:line[2],Long:line[3],Date:line[4],Confirmed:line[5],Deaths:line[6],Recovered:line[7],Active:line[8],WHO_Region:line[9]})" 
-print(session.run(query))
 
 @app.get('/')
 def home():
+    return render_template('home.html')
+
+@app.get('/data')
+def data():
     return jsonify(
         dict(
             data='Testing stuff'
